e13c5c57311be90856887c2838d401619e8941b9
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2021-11-29 07:04:40 -0500

    added widget button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import netlifyIdentity from 'netlify-identity-widget';
+
+netlifyIdentity.init();
 
 function App() {
+
+  const openWidget = () => {
+    netlifyIdentity.open('login');
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn-open" onClick={openWidget}>Open Widget</button>
     </div>
   );
 }
